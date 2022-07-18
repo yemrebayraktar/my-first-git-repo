cd8cf8312effb1c48aa0e9c3e27160d5c7152413
Emre B <yemrebayraktar88@gmail.com>
2022-07-17 23:10:31 -0400

    third commit

--- src/main/java/MyGitClass.java
+++ src/main/java/MyGitClass.java
@@ -3,6 +3,8 @@ public class MyGitClass {
     public static void main(String[] args) {
 
         System.out.println("learning Github");
+        System.out.println("adding new code");
+        System.out.println("even more ones");
     }
 
 }
